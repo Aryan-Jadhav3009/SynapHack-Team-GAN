a90ae943fb64c005b01233fb0f4dcdacf5a9fa62
Aryan-Jadhav3009 <142770981+Aryan-Jadhav3009@users.noreply.github.com>
2025-08-25 22:11:50 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,17 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
-      </head>
-      <body suppressHydrationWarning>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body suppressHydrationWarning className="font-sans">
         <ClientThemeProvider>
           <AuthProvider>{children}</AuthProvider>
         </ClientThemeProvider>
